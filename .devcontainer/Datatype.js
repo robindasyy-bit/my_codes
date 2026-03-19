2e0b699fc24c50ce6b5aa1fd4d8d1a43a8fa3318
Robin <robindasyy@gmail.com>
2026-03-19 16:50:19 +0000

    datatype

---
+++ .devcontainer/Datatype.js
@@ -0,0 +1,12 @@
+//alert("robin")
+console.log(3+3)
+
+//string = ""
+//null = no values
+//number
+//bigint
+//boolean = true/false
+//symbol = unique
+
+console.log(typeof "12") // show type
+console.log(typeof null) // object type
