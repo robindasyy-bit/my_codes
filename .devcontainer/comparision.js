03d8599c306671c7728d077bf1906d00954d5b04
Robin <robindasyy@gmail.com>
2026-03-20 19:01:20 +0000

    comparision

---
+++ .devcontainer/comparision.js
@@ -0,0 +1,11 @@
+console.log(2 < 1);
+console.log("2" < 4); // auto convert "2" into number 
+
+console.log(null < 0);
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0); // error
+console.log(null <= 0); // error
+
+// === strip check
+console.log("2" === 2);  // also check datatype
