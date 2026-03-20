5acbd085c19be61709a40e1cdf053bc728cde8d7
Robin <robindasyy@gmail.com>
2026-03-20 10:53:02 +0000

    operation

---
+++ .devcontainer/operation.js
@@ -0,0 +1,20 @@
+let num = 33
+negnum = -num
+console.log(negnum);
+
+let a = "hello"
+let b = " robin"
+c = a+b
+console.log(c);
+
+console.log( 2 + "2");
+console.log( "2" + 2);
+console.log( 3 + 2 + "3"); // conseder the first one
+
+console.log(true) // true
+console.log(+true) // 1
+console.log(+"") // 0
+
+let score = 100
+score++
+console.log(score);
