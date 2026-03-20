1787646ca68316dde0f2c0d7aa97e07036ed0b71
Robin <robindasyy@gmail.com>
2026-03-20 05:48:13 +0000

    conversion_of_datatype

---
+++ .devcontainer/conversion_of_datatype.js
@@ -0,0 +1,14 @@
+let data = undefined    // "33jfdk" = NaN 
+console.log(typeof data);
+
+//let data1 = Number(data)
+//console.log(typeof data1);
+console.log(data);
+
+let no = 20
+let boolno = Boolean(no);
+console.log(boolno);
+
+// True = 1 , 29 , "robin"
+// False = 0 , "" 
+
